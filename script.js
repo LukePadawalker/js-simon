8639789858eea816535ac34cfe8ac41ba38ee659
LukePadawalker <padurariu.luca@gmail.com>
2024-01-12 16:47:20 +0100

    counter finished

--- script.js
+++ script.js
@@ -48,7 +48,9 @@ console.log("js ok");
 
 
 // - prendiamo la cella (secondsContainer) che contiene il numero dei secondi dal dom e la incapsuliamo dentro una variabile
-const secondsContainer = document.getElementById('secondscontainer'); 
+let secondsContainer = document.getElementById('secondscontainer'); 
+const numbersContainer = document.getElementById("numbers");
+
 
 // - creiamo un array con 5 numeri casuali;
 let numeriGiusti = [];
@@ -67,10 +69,61 @@ randomNumGenerator();
 randomNumGenerator();
 randomNumGenerator();
 
+
 // bonus: aggiungi un selettore di difficolta?
 
-// controllore 
-console.log(numeriGiusti);
+
+// creiamo una funzione che ci crea una cella
+const createCell = () => {
+  const cell = document.createElement('div');
+  return cell;
+}
+
+// - per ogni numero dentro l'array numeriGiusti, creiamo una cella, aggiungiamo il numero aggiungiamo la cella in pagina
+for (let i = 0;i < numeriGiusti.length; i++){
+
+  // creiamo la cella
+  newCell = createCell();
+
+  // inseriamo dentro la cella il numero dell'array
+  newCell.innerText = numeriGiusti[i];
+
+  // inseriamo la cella in pagina 
+  numbersContainer.appendChild(newCell);
+
+}
+
+// - creiamo il timer:
+// - creiamo una variabile const secondi = 30;
+// - creiamo una funzione ( const timerchanger = setInterval() )che ogni 1000ms esegua una funzione:
+//   - modifichiamo il testo di secondsContainer e ci mettiamo dentro --secondi;
+//   if (secondi <= 0){
+//      clearInterval(timerchanger)
+//      return;
+//   }
+
+
+// - creiamo una variabile secondi;
+let seconds = 30;
+secondsContainer.innerText = seconds;
+
+
+// - creiamo una funzione timerchanger che ogni 1000ms diminuisca il numero di 1:
+let timerChanger = () => {
+  secondsContainer.innerText = --seconds;
+  return secondsContainer;
+}
+
+setInterval(timerChanger, 1000);
+
+
+
+
+
+
+
+
+
 
 
 
