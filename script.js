8468e787672e46b45d7129779633418b0abc62ce
LukePadawalker <padurariu.luca@gmail.com>
2024-01-12 15:30:22 +0100

    array with random numbers created

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 console.log("js ok");
 
 /*
+
  - prendiamo la cella (secondsContainer) che contiene il numero dei secondi dal dom e la incapsuliamo dentro una variabile
+
  - creiamo un array con 5 numeri casuali;
    let numeriGiusti = [];
+
  - creiamo una funzione che genera un numero random e che inseriche il numero dentro l'array;
+
  - chiamiamo 5 volte questa funzione
+
  - per ogni numero dentro l'array numeriGiusti, creiamo una cella, aggiungiamo il numero aggiungiamo la cella in pagina
+
  - creiamo il timer:
    - creiamo una variabile const secondi = 30;
    - creiamo una funzione ( const timerchanger = setInterval() )che ogni 1000ms esegua una funzione:
@@ -15,8 +21,10 @@ console.log("js ok");
         clearInterval(timerchanger)
         return;
      }
+
  - creiamo un array che conterra i numeri che l'utente ci ha dato
    let numUtente = [];
+
  - una volta che il timer è scaduto, chiediamo all'utente i cinque numeri uno alla volta con dei prompt
    const num1 = prompt("inserisci il numero 1");
    const num2 = prompt("inserisci il numero 2");
@@ -33,16 +41,44 @@ console.log("js ok");
      
    }
 
-   
  - mostriamo all'utente il risultato finale
 
-
-
-   
-
-
-
-
 */
+
+
+
+// - prendiamo la cella (secondsContainer) che contiene il numero dei secondi dal dom e la incapsuliamo dentro una variabile
+const secondsContainer = document.getElementById('secondscontainer'); 
+
+// - creiamo un array con 5 numeri casuali;
+let numeriGiusti = [];
+
+// - creiamo una funzione che genera un numero random e che inserisce il numero dentro l'array;
+const randomNumGenerator = () => {
+    const randomNum = Math.floor(Math.random() * 100);
+    numeriGiusti.push(randomNum);
+    return randomNum;
+}
+
+// - chiamiamo 5 volte questa funzione
+randomNumGenerator();
+randomNumGenerator();
+randomNumGenerator();
+randomNumGenerator();
+randomNumGenerator();
+
+// bonus: aggiungi un selettore di difficolta?
+
+
+console.log(numeriGiusti);
+
+
+
+
+
+
+
+
+
 
 
